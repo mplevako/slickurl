46f3a8d7e038e1bcc2fa88c56b8afa7669fe2ff9
mplevako <mplevako@gmail.com>
2014-09-18 17:05:30 +0400

    Add URL codec

---
+++ src/test/scala/shortenurl/urlcodec/URLCodecSpec.scala
@@ -0,0 +1,31 @@
+package shortenurl.urlcodec
+
+import org.specs2.mutable.Specification
+
+class URLCodecSpec extends Specification {
+  "URL codec" should {
+    "encode correctly" in {
+      URLCodec.encode(alphabet, Long.MinValue) must beEmpty
+      URLCodec.encode(alphabet, 0) must_== "a"
+      URLCodec.encode(alphabet, alphabet.length) must_== encodedAlphabetLength
+      URLCodec.encode(alphabet, Int.MaxValue) must_== encodedIntMaxVal
+      URLCodec.encode(alphabet, Long.MaxValue) must_== encodedLongMaxVal
+      URLCodec.encode(alphabet, Int.MaxValue * 3 ) must_== encodedThreeTimesMaxInt
+    }
+
+    "decode correctly" in {
+      URLCodec.decode(alphabet, null) must_== -1
+      URLCodec.decode(alphabet, "") must_== -1
+      URLCodec.decode(alphabet, "a") must_== 0
+      URLCodec.decode(alphabet, encodedAlphabetLength) must_== alphabet.length
+      URLCodec.decode(alphabet, encodedIntMaxVal) must_== Int.MaxValue
+      URLCodec.decode(alphabet, encodedThreeTimesMaxInt) must_== Int.MaxValue * 3
+    }
+  }
+
+  val alphabet                = "abcdefghijkmnpqrstuvwxyzABCDEFGHIJKLMNPQRSTUVWXYZ23456789"
+  val encodedAlphabetLength   = alphabet.charAt(1).toString ++ alphabet.charAt(0).toString
+  val encodedIntMaxVal        = "dIA5IR"
+  val encodedLongMaxVal       = "BDjzTwjgkf8"
+  val encodedThreeTimesMaxInt = "dIA5IP"
+}
