f8acbf97b9e517b330c86893aacd6952cea632a5
mplevako <mplevako@gmail.com>
2014-09-18 13:23:24 +0400

    Split locations of the user and folder databases

--- src/main/scala/shortenurl/ShortenerApp.scala
+++ src/main/scala/shortenurl/ShortenerApp.scala
@@ -12,39 +12,52 @@ import spray.can.Http
 import scala.slick.driver.JdbcProfile
 import scala.slick.jdbc.meta.MTable
 
-object ShortenerApp extends App with UserRepositoryComponent   with UserTable
-                                with FolderRepositoryComponent with FolderTable {
+object ShortenerApp extends App {
   val config = ConfigFactory.load()
+
+  //start the cluster
+  implicit val system = ActorSystem("ShortenerSystem", config)
+  new UserRepositoryApp
+  new FolderRepositoryApp()
+
+  //start the server
+  val mediator = DistributedPubSubExtension(system).mediator
+  val shorteningService = system.actorOf(Props(classOf[Shortener], mediator).withRouter(RoundRobinRouter(nrOfInstances = 1)), name = "shortener")
+  IO(Http) ! Http.Bind(shorteningService, interface = config.getString("app.http.server.if"), port = config.getInt("app.http.server.port"))
+}
+
+class UserRepositoryApp(implicit val system: ActorSystem) extends UserRepositoryComponent
+                                                              with UserTable {
 
   override val profile: JdbcProfile = scala.slick.driver.PostgresDriver
   override val db: JdbcProfile#Backend#Database = profile.simple.Database.forConfig("db.users")
   override val userRepository: UserRepository = new UserRepositoryImpl
-  override val folderRepository: ShortenerApp.FolderRepository = new FolderRepositoryImpl
 
-  import shortenurl.ShortenerApp.profile.simple._
+  import profile.simple._
   db withSession { implicit session =>
     if (MTable.getTables("USER").list.isEmpty) {
       users.ddl.create
     }
   }
 
+  //start the user repo
+  system.actorOf(Props(classOf[UserRepoFactory], userRepository), name = "user-repo")
+}
+
+class FolderRepositoryApp(implicit val system: ActorSystem) extends FolderRepositoryComponent
+                                                                    with  FolderTable {
+
+  override val profile: JdbcProfile = scala.slick.driver.PostgresDriver
+  override val db: JdbcProfile#Backend#Database = profile.simple.Database.forConfig("db.folders")
+  override val folderRepository: FolderRepository = new FolderRepositoryImpl
+
+  import profile.simple._
   db withSession { implicit session =>
     if (MTable.getTables("FOLDER").list.isEmpty) {
       folders.ddl.create
     }
   }
 
-  //start the cluster
-  implicit val system = ActorSystem("ShortenerSystem", config)
-
-  //start the user repo
-  system.actorOf(Props(classOf[UserRepoFactory], userRepository), name = "user-repo")
-
   //start the folders repo
   system.actorOf(Props(classOf[FolderRepoFactory], folderRepository), name = "folder-repo")
-
-  //start the server
-  val mediator = DistributedPubSubExtension(system).mediator
-  val shorteningService = system.actorOf(Props(classOf[Shortener], mediator).withRouter(RoundRobinRouter(nrOfInstances = 1)), name = "shortener")
-  IO(Http) ! Http.Bind(shorteningService, interface = config.getString("app.http.server.if"), port = config.getInt("app.http.server.port"))
 }
